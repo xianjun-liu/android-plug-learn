0c10aff2455b5e5ad4c2f8e629cf0c09566c7f2e
佟亚鹏 <yapeng.tong@baidao.comc>
2016-07-06 23:50:13 +0800

    add runplug2

---
+++ RunPlugActivity2/plug1/src/main/java/com/tong/plug1/MainActivity.java
@@ -0,0 +1,20 @@
+package com.tong.plug1;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_main);
+//		TextView tv = new TextView(this);
+//		tv.setText("哈哈哈哈哈");
+//		tv.setBackgroundColor(getResources().getColor(android.R.color.black));
+//
+//		tv.setTextColor(getResources().getColor(android.R.color.white));
+//
+//		setContentView(tv,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+	}
+}
